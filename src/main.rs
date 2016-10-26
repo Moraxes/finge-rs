269babcf27be34d8a2c0398c84d241714a15ae41
Maciej Śniegocki <m.w.sniegocki@gmail.com>
2016-10-26 21:39:15 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#![feature(proc_macro)]
+
+extern crate bincode as bc;
+extern crate clap;
+extern crate rand;
+extern crate serde;
+extern crate serde_json as sj;
+#[macro_use] extern crate serde_derive;
+
+mod nn;
+mod program_args;
+
+fn main() {
+  let args = program_args::get();
+}
---
+++ src/nn.rs
@@ -0,0 +1,109 @@
+use ::rand;
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Network {
+  pub layer_sizes: Vec<usize>,
+  pub activation_coeffs: Vec<f32>,
+  pub weights: Vec<Vec<f32>>,
+}
+
+#[derive(Serialize, Deserialize, Clone, Copy)]
+pub struct TrainConfig {
+  pub learning_rate: f32,
+  pub momentum_rate: f32,
+  pub validation_ratio: f32,
+  pub sequential_validation_failures_required: usize,
+  pub max_epochs: Option<usize>,
+}
+
+pub type TrainData = Vec<(Vec<f32>, Vec<f32>)>;
+
+impl Network {
+  pub fn from_definition(layer_sizes: Vec<usize>, activation_coeffs: Vec<f32>) -> Network {
+    Network {
+      layer_sizes: layer_sizes.clone(),
+      activation_coeffs: activation_coeffs,
+      weights: layer_sizes.windows(2).map(|w| {
+        let mut v = Vec::with_capacity(w[0] * w[1]);
+        unsafe { v.set_len(w[0] * w[1]) };
+        v
+      }).collect::<Vec<_>>(),
+    }
+  }
+
+  pub fn assign_random_weights<R: rand::Rng>(&mut self, rng: &mut R) {
+    use rand::distributions::{Normal, IndependentSample};
+
+    let weight_dist = Normal::new(0., 1.);
+    for matrix in &mut self.weights {
+      for weight in matrix.iter_mut() {
+        *weight = weight_dist.ind_sample(rng) as f32;
+      }
+    }
+  }
+
+  pub fn train(&mut self, train_data: TrainData, validation_data: TrainData, conf: &TrainConfig) {
+    let mut epochs_since_validation_improvement = 0usize;
+    let mut layers = (0..(self.layer_sizes.len())).map(|it| {
+      let mut v = Vec::with_capacity(self.layer_sizes[it]);
+      unsafe { v.set_len(self.layer_sizes[it]); }
+      v
+    }).collect::<Vec<_>>();
+
+    while epochs_since_validation_improvement < conf.sequential_validation_failures_required {
+      for &(ref input, ref output) in &train_data {
+        layers[0] = input.clone();
+        self.feed_forward(&mut layers);
+        let out_layer_err = layers.last().unwrap()
+          .iter()
+          .zip(output)
+          .map(|(y, o)| o - y)
+          .collect::<Vec<_>>();
+        self.backpropagate(&mut layers, &out_layer_err, conf);
+      }
+    }
+  }
+
+  fn feed_forward(&mut self, layers: &mut Vec<Vec<f32>>) {
+    unimplemented!()
+  }
+
+  fn backpropagate(&mut self, layers: &mut Vec<Vec<f32>>, out_layer_err: &[f32], conf: &TrainConfig) {
+    unimplemented!()
+  }
+
+  pub fn write(&self, filename: &str) {
+    use ::{std, bc};
+    use std::error::Error;
+    use std::fs::File;
+    use std::io::Write;
+
+    let bytes = bc::serde::serialize(self, bc::SizeLimit::Infinite)
+      .unwrap_or_else(
+        |err| {
+          let _ = writeln!(
+            std::io::stderr(),
+            "bincode serialization error: {}\n{}",
+            err.description(),
+            err.cause().map(Error::description).unwrap_or(""));
+        Vec::new()
+      });
+    
+    let mut file: File = match File::create(filename) {
+      Err(err) =>
+        panic!(
+          "failed to create file {}: {}\n{}",
+          filename,
+          err.description(),
+          err.cause().map(Error::description).unwrap_or("")),
+      Ok(f) => f,
+    };
+
+    file.write_all(&bytes).unwrap_or_else(
+      |err| panic!(
+        "failed to write to file {}: {}\n{}",
+        filename,
+        err.description(),
+        err.cause().map(Error::description).unwrap_or("")));
+  }
+}
